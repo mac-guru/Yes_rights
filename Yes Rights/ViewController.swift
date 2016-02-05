3d8b74980f31c6fc0f246d98f95f8b338d883dce
mac-guru <amritsapkota11@gmail.com>
2016-02-05 17:07:25 +0545

    Initial Commit

---
+++ Yes Rights/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Yes Rights
+//
+//  Created by amrit on 2/5/16.
+//  Copyright © 2016 Amrit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func clickMe(sender: AnyObject) {
+        print("Button Click")
+    }
+
+}
+
